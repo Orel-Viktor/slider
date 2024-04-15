f02738e0db7563087fe65cfe4b9426b562f187cc
Orel-Viktor <orelviktor93@gmail.com>
2024-04-15 16:10:41 +0300

    delete wiper

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
 import "./style.css";
-
-import Swiper from "swiper/bundle";
-
-// import styles bundle
-import "swiper/css/bundle";
-
-const swiper = new Swiper(".swiper", {
-  loop: true,
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
