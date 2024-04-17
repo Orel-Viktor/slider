fa33c5d9a489c32d9c5cfca27f5edec6ddb466f4
Orel-Viktor <orelviktor93@gmail.com>
2024-04-17 14:09:54 +0300

    add desktop slider

--- main.js
+++ main.js
@@ -1,47 +1,4 @@
 import "./style.css";
 import { slider } from "./swiper";
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const prevBtn = document.querySelector(".swiper__prev");
-//   const nextBtn = document.querySelector(".swiper__next");
-//   const slides = document.querySelectorAll(".swiper__item");
-//   console.log(slides[0]);
-
-//   let currentSlideIndex = 2;
-
-//   function showSlide(index) {
-//     slides.forEach((slide, i) => {
-//       const description = slide.querySelector(".swiper__item-description");
-//       const profileImg = slide
-//         .querySelector(".swiper__item-image")
-//         .querySelector("img");
-
-//       if (i === index) {
-//         description.classList.add("active");
-//         profileImg.classList.add("active");
-//       } else {
-//         description.classList.remove("active");
-//         profileImg.classList.remove("active");
-//       }
-//     });
-//   }
-
-//   function prevSlide() {
-//     currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-//     showSlide(currentSlideIndex);
-//     console.log("prev");
-//   }
-
-//   function nextSlide() {
-//     currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-//     showSlide(currentSlideIndex);
-//     console.log("prev");
-//   }
-
-//   prevBtn.addEventListener("click", prevSlide);
-//   nextBtn.addEventListener("click", nextSlide);
-
-//   showSlide(currentSlideIndex);
-// });
-
 document.addEventListener("DOMContentLoaded", slider());
--- swiper.js
+++ swiper.js
@@ -5,12 +5,10 @@ export function slider() {
   const swiperContent = document.querySelector(".swiper__wrapper");
 
   const curentIndex = 3;
-  const nextSlide = (curentIndex) => {
-    console.log("click");
+
+  const prevSlide = (curentIndex) => {
     const lastElement = slides.pop();
-    console.log(lastElement);
     slides.unshift(lastElement);
-    console.log(slides);
     slides.forEach((slide, i) => {
       const description = slide.querySelector(".swiper__item-description");
       const profileImg = slide
@@ -25,12 +23,35 @@ export function slider() {
       }
     });
   };
-
+  const nextSlide = (curentIndex) => {
+    const firstElement = slides.shift();
+    slides.push(firstElement);
+    slides.forEach((slide, i) => {
+      const description = slide.querySelector(".swiper__item-description");
+      const profileImg = slide
+        .querySelector(".swiper__item-image")
+        .querySelector("img");
+      if (i === curentIndex) {
+        description.classList.add("active");
+        profileImg.classList.add("active");
+      } else {
+        description.classList.remove("active");
+        profileImg.classList.remove("active");
+      }
+    });
+  };
   prevBtn.addEventListener("click", () => {
-    nextSlide(curentIndex);
+    prevSlide(curentIndex);
     const lastSlide = swiperContent.lastElementChild;
     swiperContent.removeChild(lastSlide);
     swiperContent.insertBefore(lastSlide, swiperContent.firstElementChild);
   });
-  nextSlide(curentIndex);
+
+  nextBtn.addEventListener("click", () => {
+    nextSlide(curentIndex);
+    const firstSlide = swiperContent.firstElementChild;
+    swiperContent.removeChild(firstSlide);
+    swiperContent.appendChild(firstSlide);
+  });
+  prevSlide(curentIndex);
 }
